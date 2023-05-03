281182b40f31fc35751d2e737a996f386960d840
Ivan <serbakovv79@gmail.com>
2023-05-03 23:05:03 +0400

    fix header

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm("Fre you here?");
+// console.log(result);
+
+// const answer = prompt("Вам  еесть 18?", "18");
+// console.log(answer);
+
+ const answers = []; //создаем массив
+ answers[0] = prompt('Как ваше имя?', '');
+ answers[1] = prompt('Как ваше фамилия?', '');
+ answers[2] = prompt('Сколько вам лет?', '');
+
+
+document.write(answers); //чтобы вводилось прям на странице(видел пользователь)
