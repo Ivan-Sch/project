47489cf21f464841e93e8b157d88ec24785810ee
Ivan <serbakovv79@gmail.com>
2023-05-06 13:03:46 +0400

    add gitig

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 "use sctrict";
 
-// alert('Hello');
-
-// const result = confirm("Fre you here?");
-// console.log(result);
-
-// const answer = prompt("Вам  еесть 18?", "18");
-// console.log(answer);
-
- const answers = []; //создаем массив
- answers[0] = prompt('Как ваше имя?', '');
- answers[1] = prompt('Как ваше фамилия?', '');
- answers[2] = prompt('Сколько вам лет?', '');
-
-
-document.write(answers); //чтобы вводилось прям на странице(видел пользователь)
+let number0Films = prompt('Скольк офильмов вы уже посмотрели?', '');
+console.log( number0Films);
