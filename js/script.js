2b3710f0a27d289844c8f46bf8234a23489b4b89
Ivan <serbakovv79@gmail.com>
2023-05-06 13:36:11 +0400

    Урок 12

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 "use sctrict";
 
-let number0Films = prompt('Скольк офильмов вы уже посмотрели?', '');
-console.log( number0Films);
+const number0Films = +prompt('Скольк офильмов вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+    count: number0Films,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из послдений просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из послдений просмотренных фильмов", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
