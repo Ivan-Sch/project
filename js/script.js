36ba86e4cf571666f814dc79c233f0c45504e023
Ivan <serbakovv79@gmail.com>
2023-05-08 13:26:43 +0400

    Avtomatisition

--- js/script.js
+++ js/script.js
@@ -11,12 +11,32 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из послдений просмотренных фильмов", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из послдений просмотренных фильмов", ""),
-      d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Один из послдений просмотренных фильмов", ""),
+        b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("error");
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
 
 console.log(personalMovieDB);
